01f1945dbd0d3c69ef404f877256b7ce59f67646
AlphaDweb <savanthhemanth@gmail.com>
2025-08-27 13:45:46 +0530

    Updated file

--- src/main/java/com/example/notes/model/User.java
+++ src/main/java/com/example/notes/model/User.java
@@ -1,12 +1,14 @@
 package com.example.notes.model;
 
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Document(collection = "users")
 public class User {
     @Id
     private String id;
+    @Indexed(unique = true)
     private String username;
     private String password;
     // Additional registration details:
